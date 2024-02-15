f784a1774f9a9b0c404eda2bc78d244fbdb0e78c
Max GurunG <maxgurung@Maxs-MacBook-Pro.local>
2024-02-14 20:09:53 -0500

    Make folder structure

---
+++ assignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x;
+        Console.Write("Enter a real number: "); // Prompt the user to enter a real number
+        while (!double.TryParse(Console.ReadLine(), out x)) // Repeat until valid input is received
+        {
+            Console.WriteLine("Invalid input. Please enter a valid real number."); // Display error message for invalid input
+            Console.Write("Enter a real number(x): "); // Prompt the user again for input
+        }
+
+        // Check  x in the set I: [2, 3[ U [0, 1[ U [-10, -2]
+        if ((x >= 2 && x < 3) || (x > 0 && x <= 1) || (x >= -10 && x <= -2))
+        {
+            Console.WriteLine("x belongs to I"); // Display message if x belongs to I
+        }
+        else
+        {
+            Console.WriteLine("x does not belong to I"); // Display message if x does not belong to I
+        }
+    }
+}
+
